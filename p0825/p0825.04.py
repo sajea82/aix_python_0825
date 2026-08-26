93c951ec0b9276b2500a384440f0dc2fbb789ce5
sajea82@gmail.com <sajea82@gmail.com>
2026-08-26 10:24:48 +0900

    파이썬수업-print,변수,타입

---
+++ p0825/p0825.04.py
@@ -0,0 +1,51 @@
+# 타입 확인
+a = 100
+b = 10.1
+c = "안녕"
+d = True
+# c가 무슨타입?
+# 타입 확인방법 type(a)
+print(type(a))
+print(type(b))
+print(type(c))
+print(type(d))
+
+# a = 9
+# b = 4
+# print(a+b)
+# print(a-b)
+# print(a*b)
+# print(a/b)
+# print(a//b) #몫 2
+# print(a%b)  #나머지 0
+# print(a**b) #제곱100000
+
+
+# print(10+5)
+# print(10-5)
+# print(10*5)
+# print(10/5)
+# print(10//5) #몫 2
+# print(10%5)  #나머지 0
+# print(10**5) #제곱100000
+
+
+# print(9+4)
+# print(9-4)
+# print(9*4)
+# print(9/4)
+# print(9//4)
+# print(9%4)
+# print(9**4)
+
+
+# # 변수 : 값을 저장하는 공간, 타입은 값을 입력할때 정해짐.
+# # 타입 : 4개 숫자형타입(정수타입,실수타입),문자영타입,불타입
+# a = 10      # 숫자형타입 - 정수타입
+# b = 10.1    # 숫자형타입 - 실수타입, 소수점
+# aa = "안녕"  # 문장열타입
+# abc = True  # 불타입(boo1) - True, False / boolean
+
+# 예약어는 변수로 사용할수 없음.
+# True = 1
+print = 5
