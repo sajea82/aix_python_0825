56a3bf6c8b6b93541a757bdcf28342acdbafaaad
LKH <sajea82@gmail.com>
2026-08-27 00:40:34 +0900

    연습

--- p0826/p0826/p0826_08.py
+++ p0826/p0826/p0826_08.py
@@ -30,14 +30,36 @@
 # print("-"*60)
 
 # 1 김철수 100 100 100
+# no = input("번호 입력>>")
+# name = input("이름 입력>>")
+# kor = int(input("국어점수 입력>>"))
+# eng = int(input("영어점수 입력>>"))
+# math = int(input("수학점수 입력>>"))
+# total = kor+eng+math
+# avg = total/3
+# print("-"*60)
+# print("번호\t이름\t 국어\t 영어\t 수학\t 합계\t 평균")
+# print("{}\t{}\t{}\t{}\t{}\t{}\t{:.2f}".format(no,name,kor,eng,math,total,avg))
+# print("-"*60)
+
+# 1 박영훈 100 80 75 90
 no = input("번호 입력>>")
 name = input("이름 입력>>")
 kor = int(input("국어점수 입력>>"))
 eng = int(input("영어점수 입력>>"))
 math = int(input("수학점수 입력>>"))
-total = kor+eng+math
-avg = total/3
+sci = int(input("과학점수 입력>>"))
+total = kor+eng+math+sci
+avg = total/4
 print("-"*60)
-print("번호\t이름\t 국어\t 영어\t 수학\t 합계\t 평균")
-print("{}\t{}\t{}\t{}\t{}\t{}\t{:.2f}".format(no,name,kor,eng,math,total,avg))
+print("번호\t이름\t국어\t영어\t수학\t과학\t합계\t평균")
+print("{}\t{}\t{}\t{}\t{}\t{}\t{}\t{:.2f}".format(no,name,kor,eng,math,sci,total,avg))
 print("-"*60)
+print("안녕")
+
+
+
+
+
+
+
