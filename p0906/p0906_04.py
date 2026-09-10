a42ab199a2f6aa293f15d77948516873e51533da
sajea82@gmail.com <sajea82@gmail.com>
2026-09-10 17:46:26 +0900

    학생성적

---
+++ p0906/p0906_04.py
@@ -0,0 +1,12 @@
+stu = []
+sno = 1
+
+while True:
+    print("[ 학생성적프로그램 ]")
+    print("1. 학생성적입력")
+    print("2. 학생성적출력")
+    print("3. 학생성적수정")
+    print("4. 학생성적저장")
+    print("0. 프로그램종료")
+    print("-"*60)
+    choice = input()
