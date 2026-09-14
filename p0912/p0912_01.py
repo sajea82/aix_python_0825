a85589dedde6967e75aa7190a35ecd201ad2cd66
sajea82@gmail.com <sajea82@gmail.com>
2026-09-14 17:04:35 +0900

    챗지피티와 공부

--- p0912/p0912_01.py
+++ p0912/p0912_01.py
@@ -1,8 +1,13 @@
+pass_count = 0
+pass_total = 0
 
-    
+for i in range(1,6):
+    score = int(input(f"{i}번째 점수: "))
+    if score >= 60:
+        pass_count +=1
+        pass_total += score 
 
-
-
-
+print(f"합격자수 : {pass_count}")
+print(f"합격자 점수 합계 : {pass_total}")
 
 
